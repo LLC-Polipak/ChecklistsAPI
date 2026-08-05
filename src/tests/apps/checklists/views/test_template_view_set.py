5fed524133dc1a55e33dcfe6d18ee223d7520069
klim <klim22837@gmail.com>
2026-08-05 10:15:36 +0300

    temp

--- src/tests/apps/checklists/views/test_template_view_set.py
+++ src/tests/apps/checklists/views/test_template_view_set.py
@@ -1,8 +1,41 @@
-# from ....intagration_test_app import BaseAPITest
+
 import pytest
+from rest_framework.reverse import reverse
+from rest_framework.test import APIClient
 
 @pytest.mark.django_db
-class TestTemplateViewSet:
+class TestTemplateViewSet():
 
-    def test_200(self):
+    def setup_method(self):
         pass
+
+    def test_200(self, client):
+        url = reverse('template-list')
+        data = {
+          "equipment_uid": "EQ-FORKLIFT-01",
+          "checklist_type": "INSPECTION",
+          "fields": [
+            {
+              "name": "Уровень масла",
+              "field_type": "CHOICE",
+              "order": 1,
+              "choices": [
+                {"value": "В норме", "order": 1},
+                {"value": "Ниже минимума", "order": 2}
+              ]
+            },
+            {
+              "name": "Давление в шинах (Атм)",
+              "field_type": "INTEGER",
+              "order": 2
+            },
+            {
+              "name": "Сигналка работает?",
+              "field_type": "CHECKBOX",
+              "order": 3
+            }
+          ]
+        }
+        response = client.post(url, data)
+
+        assert response.status_code == 200
